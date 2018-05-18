5af3588d6fee0ccad374d9a3af1ed2c6023f21f4
Ronald <ronaldpatino@yahoo.com>
2018-05-18 12:55:46 -0500

    Changing parameters

--- d.py
+++ d.py
@@ -2,14 +2,16 @@ from doom.game import play
 
 
 class P:
-    def __init__(self, model, config, scenario, episodes, n_steps=0, memory=0):
+    def __init__(self, model, config, scenario, episodes=10, n_steps=0, memory=0, skip_learning=False, load_model=False):
         self.model = model
         self.config = config
         self.scenario = scenario
         self.episodes = episodes
         self.n_steps = n_steps
         self.memory = memory
+        self.skip_learning = skip_learning
+        self.load_model = load_model
 
 
-p = P("dqnold", "scenarios/basic.cfg", "basic", 10)
+p = P("dqn", config="scenarios/rocket_basic.cfg", scenario="basic", skip_learning=False, load_model=True)
 play(p)
